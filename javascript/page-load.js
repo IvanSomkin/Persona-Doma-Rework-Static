fc02f42b40c2d55b4493db630897a5d4542a3f07
Ivan Somkin <somkin.van@yandex.ru>
2022-03-23 21:10:54 +0300

    Update style.css
    Switch header ids to classes

--- javascript/page-load.js
+++ javascript/page-load.js
@@ -1,13 +1,13 @@
 var initial_time = new Date().getTime();
 
 var items_collection = document.getElementsByName("header")
-var items = items_collection[0].getElementsByClassName("header__item");
+var items = items_collection[0].getElementsByClassName("header__item")
 
 for (var i = 0; i < items.length; i++) {
   items[i].addEventListener("click", function() {
     var last = document.getElementsByClassName("header__item_current");
     last[0].className = last[0].className.replace(" header__item_current", "");
-    this.className += " header__item_curent";
+    this.className += " header__item_current";
   });
 }
 
